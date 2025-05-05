026170dae4016b1a35ee31a6e166b13fb5beaa4d
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-05-05 17:32:31 +0200

    upload skeleton plugin structure

---
+++ src/napari_sols_processing/__init__.py
@@ -0,0 +1,10 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+from ._writer import write_multiple, write_single_image
+
+__all__ = (
+    "write_single_image",
+    "write_multiple",
+)
